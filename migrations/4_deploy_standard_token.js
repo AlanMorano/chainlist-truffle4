3f04b3e8c97467b32be28ed65fb56352dd005c9a
Allan Morano <allan.morano@MVP.local>
2018-07-18 14:05:19 +0800

    adding frontend files to Github pages

---
+++ migrations/4_deploy_standard_token.js
@@ -0,0 +1,5 @@
+var StandardToken = artifacts.require("./StandardToken.sol");
+
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(StandardToken, "Standard Token", "STN", 18,100000000000);
+};
